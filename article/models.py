8607d5ef27154967e7e990954064b7515b818dc0
04105038 <1171602076@qq.com>
2016-12-08 01:18:42 +0800

    test

---
+++ article/models.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+from django.db import models
+from django.core.urlresolvers import reverse
+
+# Create your models here.
+class Article(models.Model) :
+    title = models.CharField(max_length = 100)  #博客题目
+    category = models.CharField(max_length = 50, blank = True)  #博客标签
+    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
+    content = models.TextField(blank = True, null = True)  
+
+    def get_absolute_url(self):
+        path = reverse('detail', kwargs={'id':self.id})
+        return "http://www.xdboy.cn:8000%s" % path
+            
+    def __str__(self) :
+        return self.title
+                    
+    class Meta:
+        ordering = ['-date_time']
